1d99f52890f8cba92de1936d9e87bb1f0c8a6eba
Jack Knight <jkknight9@gmail.com>
2018-12-31 19:03:42 -0700

    Initial Commit

---
+++ DeckofCardsC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeckofCardsC
+//
+//  Created by Jack Knight on 12/31/18.
+//  Copyright © 2018 Jack Knight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeckofCardsC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DeckofCardsC
+//
+//  Created by Jack Knight on 12/31/18.
+//  Copyright © 2018 Jack Knight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
